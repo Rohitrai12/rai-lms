25e83b2e356bf744ba2c67724542505c0674eb4e
Rohit Rai <161055240+Rohitrai12@users.noreply.github.com>
2025-06-30 15:58:41 +0500

    Verify Email with OTP

---
+++ app/(auth)/verify-request.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VerifyRequest = () => {
+  return (
+    <div>VerifyRmail</div>
+  )
+}
+
+export default VerifyRequest
---
+++ app/(auth)/verify-request/page.tsx
@@ -0,0 +1,12 @@
+import VerifyOTPModule from "@/app/modules/auth/verify-otp/Module";
+import React from "react";
+
+function Page() {
+  return (
+    <>
+      <VerifyOTPModule />
+    </>
+  );
+}
+
+export default Page;
